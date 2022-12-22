e1d21f60b1104f5db93df7fdf4843713bd926c7b
田亮 <tianliang@tianliangdeMacBook-Pro.local>
2022-12-22 17:03:48 +0800

    补充获取系统内存信息未上传文件

---
+++ src/Findx.Common/Metrics/Memory/MemoryStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Findx.Metrics.Memory
+{
+	public struct MemoryStatus
+	{
+        public uint dwLength;
+        public uint dwMemoryLoad;
+        public uint dwTotalPhys;
+        public uint dwAvailPhys;
+        public uint dwTotalPageFile;
+        public uint dwAvailPageFile;
+        public uint dwTotalVirtual;
+        public uint dwAvailVirtual;
+    }
+}
+
