31cb8ece025cb72bf3437cba75ece664d28c400e
tianliang <tianliang@qq.com>
2024-07-01 17:31:41 +0800

    Guids.NewId使用bug修复

---
+++ framework/Findx.Guids.NewId/MassTransitSequentialGuidGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Findx.Guids.NewId;
+
+public class MassTransitSequentialGuidGenerator: IGuidGenerator
+{
+    public System.Guid Create()
+    {
+        return MassTransit.NewId.NextSequentialGuid();
+    }
+}
--- framework/Findx.Guids.NewId/NewIdModule.cs
+++ framework/Findx.Guids.NewId/NewIdModule.cs
@@ -3,19 +3,18 @@ using Findx.Modularity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Findx.Guids.NewId
+namespace Findx.Guids.NewId;
+
+[Description("Findx-NewId模块")]
+public class NewIdModule : StartupModule
 {
-    [Description("Findx-NewId模块")]
-    public class NewIdModule : StartupModule
+    public override ModuleLevel Level => ModuleLevel.Framework;
+
+    public override int Order => 140;
+
+    public override IServiceCollection ConfigureServices(IServiceCollection services)
     {
-        public override ModuleLevel Level => ModuleLevel.Framework;
-
-        public override int Order => 140;
-
-        public override IServiceCollection ConfigureServices(IServiceCollection services)
-        {
-            services.Replace(new ServiceDescriptor(typeof(IGuidGenerator), typeof(Guids.SequentialGuidGenerator), ServiceLifetime.Singleton));
-            return services;
-        }
+        services.Replace(new ServiceDescriptor(typeof(IGuidGenerator), typeof(MassTransitSequentialGuidGenerator), ServiceLifetime.Singleton));
+        return services;
     }
 }
